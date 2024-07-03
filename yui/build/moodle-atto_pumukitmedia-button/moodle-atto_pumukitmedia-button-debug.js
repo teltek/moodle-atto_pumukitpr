5c04fb0a34ea336c5bd276a0f217e140c4f9f04c
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2024-07-03 10:50:46 +0200

    Use URL instead of iframe to remove blocks from Moodle security. Filter will convert URLs on iframes

--- yui/build/moodle-atto_pumukitmedia-button/moodle-atto_pumukitmedia-button-debug.js
+++ yui/build/moodle-atto_pumukitmedia-button/moodle-atto_pumukitmedia-button-debug.js
@@ -341,12 +341,7 @@ Y.namespace('M.atto_pumukitmedia').Button = Y.Base.create('button', Y.M.editor_a
             url = e.data.url;
         }
 
-        var iframe =
-            '<div class="embed-responsive embed-responsive-16by9 tv-iframe">' +
-            '<iframe class="embed-responsive-item tv-iframe-item" src="' + url +
-            '" allowfullscreen allow="microphone; camera; display-capture"></iframe>' +
-            '<span style="display: none;">Video</span>' +
-            '</div>';
+        var iframe = '<a href="' + url + '" target="_blank" class="pumukit-media-link">' + url + '</a>';
         this.get('host').insertContentAtFocusPoint(iframe);
         this.markUpdated();
     },
